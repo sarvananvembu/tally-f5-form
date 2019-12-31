1fa3e5319e8b48c25ae09d7a6ee201cb9ead484e
sarvananvembu <58412569+sarvananvembu@users.noreply.github.com>
2019-12-31 16:23:29 +0530

    Add files via upload

--- extra-scripts.js
+++ extra-scripts.js
@@ -177,9 +177,3 @@ function showCheckboxes4() {
     expanded = false;
   }
 }
-
-function submits() {
-  window.open("https://www.genesistally.com/services/need-accountant/f5-form-thank-you-page");
-  setTimeout("window.close()",1500) 
-
-}
